b03d57030720f82ed590eaaddb660d4b6692948b
werozi <wezie100@gmail.com>
2018-07-15 22:08:40 +0200

    Merged last two commits

--- src/main/java/pl/wewo/church/calendar/config/SecurityConfig.java
+++ src/main/java/pl/wewo/church/calendar/config/SecurityConfig.java
@@ -8,8 +8,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import pl.wewo.church.calendar.services.MongoUserDetailsService;
 
 @Configuration
@@ -23,25 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable()
 				.authorizeRequests()
 				.antMatchers("/", "/home").permitAll()
 				.anyRequest().authenticated()
 				.and()
-				//.httpBasic()
-				//.and()
-				//.sessionManagement().disable()
 				.formLogin()
 				.loginPage("/login")
 				.permitAll()
 				.and()
 				.logout()
 				.permitAll();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 	@Override
