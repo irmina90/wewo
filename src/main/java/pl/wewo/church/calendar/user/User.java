5a177436682c1db503be602bf20f919ed8dab60a
lasjak <lasjak@fake.email.com>
2018-07-15 16:12:58 +0200

    Changed ID type in User collection class.

--- src/main/java/pl/wewo/church/calendar/user/User.java
+++ src/main/java/pl/wewo/church/calendar/user/User.java
@@ -14,7 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
 
 	@Id
-	private ObjectId id;
+	private String id;
 	private String login;
 	private String password;
 	private String email;
